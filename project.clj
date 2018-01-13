31a60a05b7e49823cf0f1aca931d1cd0a2fd3121
Anonymous Applicant <anonymous@example.com>
2018-01-12 18:30:37 -0600

    template output

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject candidate-exercise "0.1.0-SNAPSHOT"
+  :description "An anonymous Democracy Works coding exercise"
+  :min-lein-version "2.7.1"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring "1.6.3"]
+                 [ring/ring-defaults "0.3.1"]
+                 [compojure "1.6.0"]
+                 [hiccup "1.0.5"]]
+  :plugins [[lein-ring "0.12.1"]]
+  :ring {:handler candidate-exercise.core/handler}
+  :aliases {"submit" ["run" "-m" "candidate-exercise.submit"]})
---
+++ src/candidate_exercise/core.clj
@@ -0,0 +1,16 @@
+(ns candidate-exercise.core
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.middleware.reload :refer [wrap-reload]]
+            [candidate-exercise.home :as home]))
+
+(defroutes app
+  (GET "/" [] home/page)
+  (route/resources "/")
+  (route/not-found "Not found"))
+
+(def handler
+  (-> app
+      (wrap-defaults site-defaults)
+      wrap-reload))
